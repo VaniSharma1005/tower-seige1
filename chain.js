9169faa5165d66e4aa5328a126d7fcf34a1d4d04
VaniSharma1005 <76725930+VaniSharma1005@users.noreply.github.com>
2021-06-03 16:15:16 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,28 @@
+class Chain{
+    constructor(bodyA,pointB) {
+        var options = {
+          bodyA: bodyA,
+          pointB: pointB,
+          stiffness: 0.04,
+          length: 2
+
+                     
+        }
+       this.pointB=pointB;
+       this.chain=Constraint.create(options);
+       World.add(world,this.chain);
+
+      }
+
+      fly(){
+          this.chain.bodyA=null;
+      }
+      display(){
+      if(this.chain.body){
+     var pointA=this.chain.body.position;
+     var pointB=this.pointB;
+    strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,ponitB.y);
+      }
+      }
+}
